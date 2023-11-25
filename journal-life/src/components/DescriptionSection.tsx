03f26ca13875419f1fcb966651a0d8652afb1e16
tulio1990 <trbanegas14@gmail.com>
2023-11-25 11:56:13 -0600

    Firts Upd

---
+++ journal-life/src/components/DescriptionSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DescriptionSection: React.FC = () => {
+  return (
+    <section className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Welcome a Journal of Life</h2>
+      <p>
+      Journal of Life is a place to keep your thoughts and memories. Register
+      your daily experiences and create a personal journal that you can treasure.
+      </p>
+      {/* ************************************* */}
+    </section>
+  );
+};
+
+export default DescriptionSection;
---
+++ journal-life/src/components/UserDiarySection.tsx
@@ -0,0 +1,15 @@
+// components/UserDiarySection.tsx
+
+import React from 'react';
+
+const UserDiarySection: React.FC = () => {
+  return (
+    <section className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Your Personal Diary</h2>
+      <p>Here you can start recording your thoughts and memories.</p>
+      {/* ***************************************** */}
+    </section>
+  );
+};
+
+export default UserDiarySection;
